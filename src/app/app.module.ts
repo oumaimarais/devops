3474a4bac49299db382d11b1fb32a67425a7ff01
oumaimarais <100963742+oumaimarais@users.noreply.github.com>
2025-03-05 23:21:30 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListEtudiantComponent } from './list-etudiant/list-etudiant.component';
+import { AddEtudiantComponent } from './add-etudiant/add-etudiant.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListEtudiantComponent,
+    AddEtudiantComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
